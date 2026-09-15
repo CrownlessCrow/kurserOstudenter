a01f1627115bbea3ad8bdfb12959a0610596eb4e
CrownlessCrow <martinphung91@hotmail.com>
2026-09-15 17:26:01 +0200

    lade till 1 student under unenroll, samt skapade mattekursen och en person. Får resultat att 1 läggs till i listan

--- Program.cs
+++ Program.cs
@@ -1,1 +1,5 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Course matte = new Course("Matematik", 2);
+Student Martin = new Student("Martin");
+
+anna.Enroll(matte);
+Console.WriteLine(matte.Students.Count);  
